da9af407b1913641c19cf5f1ec89143a90a1ad54
Nicky Gottlieb Hansen <nicky-g-hansen@hotmail.com>
2022-09-02 17:57:23 +0200

    initial.

---
+++ srp6test/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Numerics;
+using System.Text;
+using srp6test;
+
+Console.WriteLine("Hello, World!");
+
+
+
+var salt = new BigInteger(new byte[]
+{
+    0xCA,0xC9,0x4A,0xF3,0x2D,0x81,0x7B,0xA6,
+    0x4B,0x13,0xF1,0x8F,0xDE,0xDE,0xF9,0x2A,
+    0xD4,0xED,0x7E,0xF7,0xAB,0x0E,0x19,0xE9,
+    0xF2,0xAE,0x13,0xC8,0x28,0xAE,0xAF,0x57,
+}, false, true);
+
+var username = "USERNAME123";
+var password = "PASSWORD123";
+
+
+Console.WriteLine("Salt value is: " + salt);
+var calculatedX = Foo.CalculateX(username, password, salt);
+
+// Dummy password verifier.
+var verifierString = "870A98A3DA8CCAFE6B2F4B0C43A022A0C6CEF4374BA4A50CEBF3FACA60237DC4";
+var vBA = StringToByteArray(verifierString);
+var v = new BigInteger(vBA, true, true);
+Console.WriteLine("The value of v is: " + v);
+
+var serverPrivateKeyString = "ACDCB7CB1DE67DB1D5E0A37DAE80068BCCE062AE0EDA0CBEADF560BCDAE6D6B9";
+var serverPrivateKeyBA = StringToByteArray(serverPrivateKeyString);
+var serverPrivateKey = new BigInteger(serverPrivateKeyBA, true, true);
+Console.WriteLine("The value of server private key is: " + serverPrivateKey);
+
+// Expected => 85A204C987B68764FA69C523E32B940D1E1822B9E0F134FDC5086B1408A2BB43
+
+// TODO: stuck on calculating the server public key.
+var serverPublicKey = Foo.CalculateServerPublicKey(v, serverPrivateKey);
+
+Console.WriteLine("Server public key (original) is: " + ByteArrayToString(serverPublicKey.ToByteArray()));
+Console.WriteLine("Server public key (reversed) is: " + ByteArrayToString(serverPublicKey.ToByteArray().Reverse().ToArray()));
+
+
+
+static string ByteArrayToString(byte[] ba)
+{
+    StringBuilder hex = new StringBuilder(ba.Length * 2);
+    foreach (byte b in ba)
+        hex.AppendFormat("{0:x2}", b);
+    return hex.ToString();
+}
+
+static byte[] StringToByteArray(string hex)
+{
+    if (hex.StartsWith("0x"))
+    {
+        hex = hex.Replace("0x", "");
+    }
+    
+    return Enumerable.Range(0, hex.Length)
+        .Where(x => x % 2 == 0)
+        .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+        .ToArray();
+}
+
+Console.WriteLine("Value of X is: " + ByteArrayToString(calculatedX.ToByteArray()));
+
+// TODO verify server public key:: https://gtker.com/implementation-guide-for-the-world-of-warcraft-flavor-of-srp6/verification_values/calculate_B_values.txt
---
+++ srp6test/foo.cs
@@ -0,0 +1,152 @@
+﻿using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace srp6test;
+
+/// <summary>
+///
+/// The SRP-6 algorithm is defined used mathematical letters,
+/// the below table translates between short mathematical form and implementation name.
+///
+/// A => Client public key.
+/// a => client private key.
+/// 
+/// B => Server public key.
+/// b => server private key.
+///
+/// N => Large safe prime.
+/// g => generator (always 7)
+/// k => k value (always 3)
+/// s => salt
+/// U => username
+/// p => password
+/// v => password verifier
+/// M1 => Client proof (proof first sent by client, calculated by both)
+/// M2 => (proof first sent by server, calculated by both)
+/// M => (Client OR Server) proof
+/// S => S key (????)
+/// K => Session key
+/// </summary>
+public class Foo
+{
+
+    /// <summary>
+    /// Large safe prime represented as little endian.
+    /// </summary>
+    private static readonly BigInteger LARGE_SAFE_PRIME_LITTLE_ENDIAN = new(new byte[]
+    {
+        0xb7, 0x9b, 0x3e, 0x2a, 0x87, 0x82, 0x3c, 0xab,
+        0x8f, 0x5e, 0xbf, 0xbf, 0x8e, 0xb1, 0x01, 0x08,
+        0x53, 0x50, 0x06, 0x29, 0x8b, 0x5b, 0xad, 0xbd,
+        0x5b, 0x53, 0xe1, 0x89, 0x5e, 0x64, 0x4b, 0x89
+    });
+
+    
+    
+    /// <summary>
+    /// Large safe prime represented as big endian.
+    /// </summary>
+    private static readonly BigInteger LARGE_SAFE_PRIME_BIG_ENDIAN = new(new byte[]
+    {
+        0x89, 0x4b, 0x64, 0x5e, 0x89, 0xe1, 0x53, 0x5b,
+        0xbd, 0xad, 0x5b, 0x8b, 0x29, 0x06, 0x50, 0x53,
+        0x08, 0x01, 0xb1, 0x8e, 0xbf, 0xbf, 0x5e, 0x8f,
+        0xab, 0x3c, 0x82, 0x87, 0x2a, 0x3e, 0x9b, 0xb7,
+    });
+
+    /// <summary>
+    /// The generator should always be 7.
+    /// </summary>
+    private static readonly BigInteger GENERATOR_VALUE_ALWAYS_7 = new(7);
+
+    /// <summary>
+    /// The K value used in the algorithm should always be 3 (why? i don't know.)
+    /// </summary>
+    private static readonly BigInteger K_VALUE_ALWAYS_3 = new(3);
+
+
+    /// <summary>
+    /// Calculates the password verifier, v = g^x % N
+    /// </summary>
+    /// <param name="username">The username of the connecting client.</param>
+    /// <param name="password">The password of the connecting client.</param>
+    /// <param name="salt">Little Endian byte array of length 32.</param>
+    /// <returns>Little Endian byte array of length 32.</returns>
+    private static BigInteger CalculatePasswordVerifier(string username, string password, BigInteger salt)
+    {
+        // Calculate the X value, store as BigInteger.
+        var x = CalculateX(username, password, salt);
+        
+        return BigInteger.ModPow(GENERATOR_VALUE_ALWAYS_7, x, LARGE_SAFE_PRIME_LITTLE_ENDIAN);
+
+    }
+
+    /// <summary>
+    /// Calculates the x value which is necessary to calculate the password verifier.
+    /// </summary>
+    /// <param name="username"></param>
+    /// <param name="password"></param>
+    /// <param name="salt">Little Endian byte array of salt is expected.</param>
+    /// <returns></returns>
+    public static BigInteger CalculateX(string username, string password, BigInteger salt)
+    {
+        var U = username.ToUpper();
+        var p = password.ToUpper();
+
+        var innerToBeHashed = Encoding.ASCII.GetBytes(U + ":" + p);
+        var innerHashed = SHA1.HashData(innerToBeHashed);
+
+        var finalToBeHashed = salt.ToByteArray().Concat(innerHashed).ToArray();
+
+        // Reverse the output so it is returned as little endianness.
+        return new BigInteger(SHA1.HashData(finalToBeHashed).Reverse().ToArray());
+    }
+
+    /// <summary>
+    /// Calculates the server public key.
+    /// </summary>
+    /// <param name="passwordVerifier">Little Endian: The password verifier.</param>
+    /// <param name="serverPrivateKey">Little Endian: The server private key.</param>
+    /// <returns>Server public key in little endianness.</returns>
+    public static BigInteger CalculateServerPublicKey(BigInteger passwordVerifier, BigInteger serverPrivateKey)
+    {
+
+        var kv = K_VALUE_ALWAYS_3 * passwordVerifier;
+
+        var gbN = BigInteger.ModPow(GENERATOR_VALUE_ALWAYS_7, serverPrivateKey, LARGE_SAFE_PRIME_BIG_ENDIAN);
+
+        var B = (kv + gbN) % LARGE_SAFE_PRIME_BIG_ENDIAN;
+
+        return B;
+        
+        /*var B = (
+                    K_VALUE_ALWAYS_3 * passwordVerifier 
+                    +
+                    (BigInteger.ModPow(GENERATOR_VALUE_ALWAYS_7, serverPrivateKey, LARGE_SAFE_PRIME_LITTLE_ENDIAN))
+                    )
+                % LARGE_SAFE_PRIME_LITTLE_ENDIAN;
+
+        return B;*/
+        
+        var interim = 
+            BigInteger
+                .Multiply(K_VALUE_ALWAYS_3, passwordVerifier)
+            +
+            BigInteger
+                .ModPow(GENERATOR_VALUE_ALWAYS_7,serverPrivateKey, LARGE_SAFE_PRIME_LITTLE_ENDIAN)
+            ;
+
+        BigInteger.DivRem(interim, LARGE_SAFE_PRIME_LITTLE_ENDIAN, out var modulusResult);
+
+        return modulusResult;
+    }
+    
+    
+    // https://gtker.com/implementation-guide-for-the-world-of-warcraft-flavor-of-srp6/
+    // https://gtker.com/implementation-guide-for-the-world-of-warcraft-flavor-of-srp6/verification_values/calculate_x_values.txt
+    // https://gtker.com/implementation-guide-for-the-world-of-warcraft-flavor-of-srp6/verification_values/calculate_x_values.txt
+    // https://gtker.com/implementation-guide-for-the-world-of-warcraft-flavor-of-srp6/verification_values/calculate_x_salt_values.txt
+    // https://gtker.com/implementation-guide-for-the-world-of-warcraft-flavor-of-srp6/#world-packet-header-encryption
+    // https://gtker.com/implementation-guide-for-the-world-of-warcraft-flavor-of-srp6/#reconnecting
+}
